ee6b3c983d57c0764bdb55821b8b30ca28d80f7c
emersonjr25 <emersonjr25@hotmail.com>
2022-09-06 21:46:54 -0300

    advances in calculate hedge effect

--- Code_R/Doctoral_project_chapter_2.R
+++ Code_R/Doctoral_project_chapter_2.R
@@ -1,5 +1,5 @@
 ############ DOCTORAL CHAPTER 2 #################
-##### EMERSON CAMPOS BARBOSA JÃšNIOR ####
+##### EMERSON CAMPOS BARBOSA JÚNIOR ####
 
 ### PACKAGES ####
 library(dplyr)
@@ -9,13 +9,61 @@ dados <- read.csv("Database.csv", header = TRUE, sep = ",")
 
 subdata <- select(dados, paper_no, first_author_surname, pub_year, genus, species, population, source, trait_cat, simp_trait, T, mean)
 
-new <- 0
+unicos <- unique(subdata$paper_no)
 
-for(i in 1:length(subdata$paper_no)){
-  if(unique(subdata$paper_no[i] == subdata$paper_no))
-   new[i] <- unique(subdata$paper_no)
+pos <- list()
+
+# for(i in 1:length(unicos)){
+#   for(j in 1:length(subdata$paper_no)){
+#     pos <- which(unicos[i] == subdata$paper_no)
+#     separated_list[i] <- pos
+#   }
+# }
+
+subdata$paper_no[which(unicos[1] == subdata$paper_no)]
+
+pos <- which(unicos[1] == subdata$paper_no)
+subdata$paper_no[pos]
+subdata[pos, ]
+
+
+separated_list <- list()
+
+for(i in 1:length(unicos)){
+    pos <- which(unicos[i] == subdata$paper_no)
+    separated_list[[i]] <- subdata[pos, ]
+    pos <- 0
 }
 
-data.frame()
-unique(subdata$paper_no)
-filter()
+nova <- list()
+
+for(i in 1:length(separated_list)){
+  nova[[i]] <- unique(separated_list[[i]]$simp_trait)
+}
+
+pos <- which(nova[[1]][[1]] == separated_list[[1]]$simp_trait)
+
+separated_list[[1]]$T[pos]
+
+### TESTE ###
+unique(separated_list[[1]]$population[pos])
+unique(separated_list[[1]]$source[pos])
+
+minimo <- min(separated_list[[1]]$T[pos])
+maximo <- max(separated_list[[1]]$T[pos])
+
+posmin <- which(separated_list[[1]]$T == minimo)
+posmax <- which(separated_list[[1]]$T == maximo)
+
+
+as.numeric(separated_list[[1]]$mean)
+mean(as.numeric(separated_list[[1]]$mean[posmin]))
+mean(as.numeric(separated_list[[1]]$mean[posmax]))
+
+
+
+for(i in 1:length(unicos)){
+  for(j in 1:length(subdata$paper_no)){
+    separated_list[i] <- subdata$paper_no[j][subdata$paper_no == unicos[i]] 
+  }
+}
