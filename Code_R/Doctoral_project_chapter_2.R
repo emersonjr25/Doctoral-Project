49b854bd008e5631f2f1458ca650083910dee8ea
emersonjr25 <emersonjr25@hotmail.com>
2022-09-08 22:07:48 -0300

    advances in hedge's effect, problem with position

--- Code_R/Doctoral_project_chapter_2.R
+++ Code_R/Doctoral_project_chapter_2.R
@@ -1,9 +1,11 @@
 ############ DOCTORAL CHAPTER 2 #################
-##### EMERSON CAMPOS BARBOSA JÚNIOR ####
+##### EMERSON CAMPOS BARBOSA J?NIOR ####
 
 ### PACKAGES ####
 library(dplyr)
 library(stringr)
+library(effectsize)
+library(purrr)
 
 dados <- read.csv("Database.csv", header = TRUE, sep = ",")
 
@@ -11,21 +13,8 @@ subdata <- select(dados, paper_no, first_author_surname, pub_year, genus, specie
 
 unicos <- unique(subdata$paper_no)
 
-pos <- list()
-
-# for(i in 1:length(unicos)){
-#   for(j in 1:length(subdata$paper_no)){
-#     pos <- which(unicos[i] == subdata$paper_no)
-#     separated_list[i] <- pos
-#   }
-# }
-
-subdata$paper_no[which(unicos[1] == subdata$paper_no)]
-
-pos <- which(unicos[1] == subdata$paper_no)
-subdata$paper_no[pos]
-subdata[pos, ]
-
+#pos <- which(unicos[1] == subdata$paper_no)
+#subdata$paper_no[pos]
 
 separated_list <- list()
 
@@ -35,35 +24,63 @@ for(i in 1:length(unicos)){
     pos <- 0
 }
 
-nova <- list()
+name_traits <- list()
 
 for(i in 1:length(separated_list)){
-  nova[[i]] <- unique(separated_list[[i]]$simp_trait)
+  name_traits[[i]] <- unique(separated_list[[i]]$simp_trait)
 }
 
-pos <- which(nova[[1]][[1]] == separated_list[[1]]$simp_trait)
+only_traits <- list()
 
-separated_list[[1]]$T[pos]
+only_trait <- lapply(separated_list, FUN = function(dados){
+  logic <- names(dados) == "simp_trait"
+  posi <- which(logic == TRUE)
+  return(return(dados[posi]))
+})
 
-### TESTE ###
-unique(separated_list[[1]]$population[pos])
-unique(separated_list[[1]]$source[pos])
+out <- list()
 
-minimo <- min(separated_list[[1]]$T[pos])
-maximo <- max(separated_list[[1]]$T[pos])
+for(i in 1:length(name_traits)){
+  out[[i]] <- vector("list", length(name_traits[[i]]))
+}
+
+valor <- 0
+
+for(i in 1:length(name_traits)){
+  for(j in seq_along(name_traits[[i]])){
+    if(valor < length(name_traits[[i]])){
+      valor <- valor + 1
+      out[[i]][[valor]] <- which(name_traits[[i]][[j]] == only_trait[[1]])
+    } else {
+      valor <- 0
+      valor <- valor + 1
+      out[[i]][[valor]] <- which(name_traits[[i]][[j]] == only_trait[[1]])
+    }
+  }
+}
+
+######################################################
+
+pos_2 <- which(name_traits[[1]][[1]] == separated_list[[1]]$simp_trait)
+
+unique(separated_list[[1]]$population[pos_2])
+unique(separated_list[[1]]$source[pos_2])
+unique(separated_list[[1]]$species[pos_2])
+
+separated_list[[1]]$T[pos_2]
+
+minimo <- min(separated_list[[1]]$T[pos_2])
+maximo <- max(separated_list[[1]]$T[pos_2])
 
 posmin <- which(separated_list[[1]]$T == minimo)
 posmax <- which(separated_list[[1]]$T == maximo)
 
+mean_min <- mean(as.numeric(separated_list[[1]]$mean[posmin]))
+mean_max <- mean(as.numeric(separated_list[[1]]$mean[posmax]))
 
-as.numeric(separated_list[[1]]$mean)
-mean(as.numeric(separated_list[[1]]$mean[posmin]))
-mean(as.numeric(separated_list[[1]]$mean[posmax]))
+group_1 <- as.numeric(separated_list[[1]]$mean[posmin])
 
+group_2 <- as.numeric(separated_list[[1]]$mean[posmax])
 
+hedge_effect <- (mean_min - mean_max) / sd_pooled(group_1, group_2)
 
-for(i in 1:length(unicos)){
-  for(j in 1:length(subdata$paper_no)){
-    separated_list[i] <- subdata$paper_no[j][subdata$paper_no == unicos[i]] 
-  }
-}
