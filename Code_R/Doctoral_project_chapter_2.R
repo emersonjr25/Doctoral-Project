9076231aaedd81cfaf6bd0dfc43f715117aab6d1
emersonjr25 <emersonjr25@hotmail.com>
2022-09-09 21:00:26 -0300

    hedge's effect auto calculated, but without separated per species and traits

--- Code_R/Doctoral_project_chapter_2.R
+++ Code_R/Doctoral_project_chapter_2.R
@@ -7,28 +7,37 @@ library(stringr)
 library(effectsize)
 library(purrr)
 
+### READING DATA ###
+
 dados <- read.csv("Database.csv", header = TRUE, sep = ",")
 
 subdata <- select(dados, paper_no, first_author_surname, pub_year, genus, species, population, source, trait_cat, simp_trait, T, mean)
 
-unicos <- unique(subdata$paper_no)
+subdata2 <- paste0(subdata$genus," ", subdata$species)
 
-#pos <- which(unicos[1] == subdata$paper_no)
-#subdata$paper_no[pos]
+subdata$species_complete <- subdata2
+
+### LIST SEPARATING STUDIES ####
+
+unicos <- unique(subdata$paper_no)
 
 separated_list <- list()
 
 for(i in 1:length(unicos)){
-    pos <- which(unicos[i] == subdata$paper_no)
-    separated_list[[i]] <- subdata[pos, ]
-    pos <- 0
+  pos <- which(unicos[i] == subdata$paper_no)
+  separated_list[[i]] <- subdata[pos, ]
+  pos <- 0
 }
+
+### DISCOVERY OF POSITION OF THE TRAIT ###
 
 name_traits <- list()
 
 for(i in 1:length(separated_list)){
   name_traits[[i]] <- unique(separated_list[[i]]$simp_trait)
 }
+
+# SUB LIST #
 
 only_traits <- list()
 
@@ -38,49 +47,168 @@ only_trait <- lapply(separated_list, FUN = function(dados){
   return(return(dados[posi]))
 })
 
+### TRAITS POSITION  ###
+
 out <- list()
 
 for(i in 1:length(name_traits)){
   out[[i]] <- vector("list", length(name_traits[[i]]))
 }
 
-valor <- 0
 
 for(i in 1:length(name_traits)){
   for(j in seq_along(name_traits[[i]])){
-    if(valor < length(name_traits[[i]])){
-      valor <- valor + 1
-      out[[i]][[valor]] <- which(name_traits[[i]][[j]] == only_trait[[1]])
-    } else {
-      valor <- 0
-      valor <- valor + 1
-      out[[i]][[valor]] <- which(name_traits[[i]][[j]] == only_trait[[1]])
-    }
+    out[[i]][[j]] <- which(name_traits[[i]][[j]] == only_trait[[i]])
   }
 }
 
-######################################################
+### SPECIES POSITION ###
 
-pos_2 <- which(name_traits[[1]][[1]] == separated_list[[1]]$simp_trait)
+pos_species <- list()
 
-unique(separated_list[[1]]$population[pos_2])
-unique(separated_list[[1]]$source[pos_2])
-unique(separated_list[[1]]$species[pos_2])
+for(i in 1:length(species_per_study)){
+  pos_species[[i]] <- vector("list", length(species_per_study[[i]]))
+}
 
-separated_list[[1]]$T[pos_2]
+for(i in 1:length(species_per_study)){
+  for(j in seq_along(species_per_study[[i]])){
+    pos_species[[i]][[j]] <- which(species_per_study[[i]][[j]] == separated_list[[i]][["species_complete"]])
+  }
+}
 
-minimo <- min(separated_list[[1]]$T[pos_2])
-maximo <- max(separated_list[[1]]$T[pos_2])
+### FILTERING TO FIND HEDGE's per species in studies ###
 
-posmin <- which(separated_list[[1]]$T == minimo)
-posmax <- which(separated_list[[1]]$T == maximo)
+species_per_study <- list()
 
-mean_min <- mean(as.numeric(separated_list[[1]]$mean[posmin]))
-mean_max <- mean(as.numeric(separated_list[[1]]$mean[posmax]))
+for(i in seq_along(name_traits)){
+  for(j in seq_along(name_traits[[i]])){
+    species_per_study[[i]] <- unique(separated_list[[i]]$species_complete[out[[i]][[j]]])
+  }
+}
 
-group_1 <- as.numeric(separated_list[[1]]$mean[posmin])
+number_species <- list()
+for(i in 1:length(species_per_study)){
+  if(species_per_study[[i]] == 1){
+    number_species[[i]] <- 1
+  } else {
+    number_species[[i]] <- length(species_per_study[[i]])
+  }
+}
+only_species_with_more_1 <- 0
 
-group_2 <- as.numeric(separated_list[[1]]$mean[posmax])
+for(i in 1:length(number_species)){
+  if(number_species[[i]] == 1){
+    only_species_with_more_1[[i]] <- 0
+  } else {
+    only_species_with_more_1[i] <- number_species[[i]]
+  }
+}
 
-hedge_effect <- (mean_min - mean_max) / sd_pooled(group_1, group_2)
+only_species_with_more_1 <- only_species_with_more_1 != 0
+
+for(i in 1:length(only_species_with_more_1)){
+  if(only_species_with_more_1[i] == TRUE){
+    only_species_with_more_1[i] <- 1
+  } else {
+    only_species_with_more_1[i] <- 0
+  }
+}
+
+#for(c in seq_along(number_species)){
+  
+  #if(number_species[[c]] == 1){
+    
+    ### MINIMUM ###
+    
+    minimo <- list()
+    
+    for(i in seq_along(name_traits)){
+      for(j in seq_along(name_traits[[i]])){
+        minimo[[i]] <- min(separated_list[[i]]$T[out[[i]][[j]]])
+      }
+    }
+    
+    ### MAXIMUM ###
+    
+    maximo <- list()
+    
+    for(i in seq_along(name_traits)){
+      for(j in seq_along(name_traits[[i]])){
+        maximo[[i]] <- max(separated_list[[i]]$T[out[[i]][[j]]])
+      }
+    }
+    
+    ### MIN POSITION ###
+    
+    posmin <- list()
+    
+    for(i in seq_along(name_traits)){
+      for(j in seq_along(name_traits[[i]])){
+        posmin[[i]] <- which(separated_list[[i]]$T == minimo[[i]])
+      }
+    }
+    
+    ### MAX POSITION ###
+    
+    posmax <- list()
+    
+    for(i in seq_along(name_traits)){
+      for(j in seq_along(name_traits[[i]])){
+        posmax[[i]] <- which(separated_list[[i]]$T == maximo[[i]])
+      }
+    }
+    
+    ### MIN MEAN ###
+    
+    mean_min <- list()
+    
+    for(i in seq_along(name_traits)){
+      for(j in seq_along(name_traits[[i]])){
+        mean_min[[i]] <- mean(as.numeric(separated_list[[i]]$mean[posmin[[i]]]))
+      }
+    }
+    
+    ### MAX MEAN ###
+    
+    mean_max <- list()
+    
+    for(i in seq_along(name_traits)){
+      for(j in seq_along(name_traits[[i]])){
+        mean_max[[i]] <- mean(as.numeric(separated_list[[i]]$mean[posmax[[i]]]))
+      }
+    }
+    
+    ### GROUP 1 ###
+    
+    group_1 <- list()
+    
+    for(i in seq_along(name_traits)){
+      for(j in seq_along(name_traits[[i]])){
+        group_1[[i]] <- as.numeric(separated_list[[i]]$mean[posmin[[i]]])
+      }
+    }
+    
+    ### GROUP 2 ###
+    
+    group_2 <- list()
+    
+    for(i in seq_along(name_traits)){
+      for(j in seq_along(name_traits[[i]])){
+        group_2 [[i]] <- as.numeric(separated_list[[i]]$mean[posmax[[i]]])
+      }
+    }
+    
+    ### HEDGES's g EFFECT###
+    
+    hedge_effect <- list()
+    
+    for(i in seq_along(name_traits)){
+      for(j in seq_along(name_traits[[i]])){
+        hedge_effect[[i]] <- (mean_min[[i]] - mean_max[[i]]) / sd_pooled(group_1[[i]], group_2[[i]])
+      }
+    }
+ # } else {
+    
+  #}
+#}
 
