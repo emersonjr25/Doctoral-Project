4974bb460960c13d3c8ec2d0c908d5f9a9108bba
Emerson Junior <emersonjr25@hotmail.com>
2023-03-01 22:40:19 -0300

    testing more things to detect bug of great speciation

--- scripts/Doctoral_project_chapter_1_original.R
+++ scripts/Doctoral_project_chapter_1_original.R
@@ -46,7 +46,7 @@ if (!verify_config(config)) {
 }
 
 #### MODIFICATIONS IN CONFIG - SPECIES AND SYSTEM ####
-config$gen3sis$general$start_time <- 100
+config$gen3sis$general$start_time <- 50
 
 config$gen3sis$general$end_time <- 1
 
@@ -97,7 +97,7 @@ finalresult <- data.frame(plasticidade = runif(rep * length(plasti) * timesteps_
 # }
 # 
 # config$gen3sis$mutation$apply_evolution <- function(species, cluster_indices, landscape, config) {
-#   
+# 
 #   trait_evolutionary_power <- 0.001
 #   traits <- species[["traits"]]
 #   cells <- rownames(traits)
@@ -156,7 +156,9 @@ config$gen3sis$ecology$apply_ecology <- function(abundance, traits, landscape, c
 }
 
 loop_ecology2 <- function (config, data, vars, plasticidade) {
-  #browser()
+  # if(ti == 90){
+  # browser()
+  #     }
   if (config$gen3sis$general$verbose >= 3) {
     cat(paste("entering ecology module @ time", vars$ti, 
               "\n"))
@@ -243,98 +245,159 @@ modify_input_temperature <- function(config, data, vars, seed = 1){
   return(list(config = config, data = data, vars = vars))
 }
 
-# loop_speciation <- function (config, data, vars) 
-# {
-#   if(ti == 90){
-#     browser()
-#   }
-#   if (config$gen3sis$general$verbose >= 3) {
-#     cat(paste("entering speciation module \n"))
-#   }
-#   for (spi in 1:vars$n_sp) {
-#     species <- data$all_species[[spi]]
-#     if (!length(species[["abundance"]])) {
-#       (next)()
-#     }
-#     species_presence <- names(species[["abundance"]])
-#     if (length(species_presence) == 1) {
-#       clu_geo_spi_ti <- 1
-#     }
-#     else {
-#       distances <- config$gen3sis$dispersal$get_dispersal_values(length(species_presence), 
-#                                                                  species, data$landscape, config)
-#       permutation <- sample(1:length(species_presence), 
-#                             length(species_presence))
-#       clu_geo_spi_ti <- Tdbscan_variable(data$distance_matrix[species_presence[permutation], 
-#                                                               species_presence[permutation], drop = FALSE], 
-#                                          distances, 1)
-#       clu_geo_spi_ti <- clu_geo_spi_ti[order(permutation)]
-#     }
-#     gen_dist_spi <- decompress_divergence(species[["divergence"]])
-#     ifactor <- config$gen3sis$speciation$get_divergence_factor(species, 
-#                                                                clu_geo_spi_ti, data[["landscape"]], config)
-#     gen_dist_spi <- update_divergence(gen_dist_spi, clu_geo_spi_ti, 
-#                                       ifactor = ifactor)
-#     gen_dist_spi <- compress_divergence(gen_dist_spi)
-#     species[["divergence"]] <- gen_dist_spi
-#     clu_gen_spi_ti_c <- Tdbscan(gen_dist_spi$compressed_matrix, 
-#                                 config$gen3sis$speciation$divergence_threshold, 
-#                                 1)
-#     clu_gen_spi_ti <- clu_gen_spi_ti_c[gen_dist_spi$index]
-#     n_new_sp <- max(clu_gen_spi_ti) - 1
-#     vars$n_new_sp_ti <- vars$n_new_sp_ti + n_new_sp
-#     if (n_new_sp > 0) {
-#       if (config$gen3sis$general$verbose >= 3) {
-#         cat(paste("[!]   Wellcome Strange  Thing   [!] \n"))
-#         cat(paste(n_new_sp, "speciation event(s) happened \n"))
-#       }
-#       desc_unique <- unique(clu_gen_spi_ti)[-1] + vars$n_sp + 
-#         vars$n_sp_added_ti - 1
-#       data$phy <- rbind(data$phy, data.frame(Ancestor = rep(spi, 
-#                                                             n_new_sp), Descendent = desc_unique, Speciation.Time = rep(vars$ti, 
-#                                                                                                                        n_new_sp), Extinction.Time = rep(vars$ti, n_new_sp), 
-#                                              Speciation.Type = rep("Genetic", n_new_sp)))
-#       full_gen_dist <- gen_dist_spi
-#       gen_dist_spi$index <- gen_dist_spi$index[clu_gen_spi_ti == 
-#                                                  1]
-#       ue <- unique(gen_dist_spi$index)
-#       gen_dist_spi$compressed_matrix <- gen_dist_spi$compressed_matrix[ue, 
-#                                                                        ue, drop = FALSE]
-#       if (length(ue) > 0) {
-#         fullrange <- 1:length(ue)
-#         dimnames(gen_dist_spi$compressed_matrix) <- list(fullrange, 
-#                                                          fullrange)
-#         for (i in 1:length(gen_dist_spi$index)) {
-#           gen_dist_spi$index[i] <- fullrange[ue == gen_dist_spi$index[i]]
-#         }
-#       }
-#       for (desci in desc_unique) {
-#         tep_clu_gen_desci_index <- which(desc_unique == 
-#                                            desci) + 1
-#         new_species <- create_species_from_existing(species, 
-#                                                     desci, names(species[["abundance"]][clu_gen_spi_ti == 
-#                                                                                           tep_clu_gen_desci_index]), config)
-#         data$all_species <- append(data$all_species, 
-#                                    list(new_species))
-#       }
-#       species <- limit_species_to_cells(species = species, 
-#                                         cells = names(species[["abundance"]][clu_gen_spi_ti == 
-#                                                                                1]))
-#       vars$n_sp_added_ti <- vars$n_sp_added_ti + n_new_sp
-#       clu_geo_spi_ti <- clu_geo_spi_ti[clu_gen_spi_ti == 
-#                                          1]
-#     }
-#     data$all_species[[spi]] <- species
-#   }
-#   if (config$gen3sis$general$verbose >= 3) {
-#     cat(paste("exiting speciation module \n"))
-#   }
-#   if (config$gen3sis$general$verbose >= 3 && vars$n_sp_added_ti > 
-#       0) {
-#     cat(paste(vars$n_sp_added_ti, "new species created \n"))
-#   }
-#   return(list(config = config, data = data, vars = vars))
-# }
+################### FOR TESTS ####################################
+
+loop_speciation <- function (config, data, vars)
+{
+  # if(ti == 94){
+  #   browser()
+  # }
+  if (config$gen3sis$general$verbose >= 3) {
+    cat(paste("entering speciation module \n"))
+  }
+  for (spi in 1:vars$n_sp) {
+    species <- data$all_species[[spi]]
+    if (!length(species[["abundance"]])) {
+      (next)()
+    }
+    species_presence <- names(species[["abundance"]])
+    if (length(species_presence) == 1) {
+      clu_geo_spi_ti <- 1
+    }
+    else {
+      distances <- config$gen3sis$dispersal$get_dispersal_values(length(species_presence),
+                                                                 species, data$landscape, config)
+      permutation <- sample(1:length(species_presence),
+                            length(species_presence))
+      clu_geo_spi_ti <- Tdbscan_variable(data$distance_matrix[species_presence[permutation],
+                                                              species_presence[permutation], drop = FALSE],
+                                         distances, 1)
+      clu_geo_spi_ti <- clu_geo_spi_ti[order(permutation)]
+    }
+    gen_dist_spi <- decompress_divergence(species[["divergence"]])
+    ifactor <- config$gen3sis$speciation$get_divergence_factor(species,
+                                                               clu_geo_spi_ti, data[["landscape"]], config)
+    gen_dist_spi <- update_divergence(gen_dist_spi, clu_geo_spi_ti,
+                                      ifactor = ifactor)
+    gen_dist_spi <- compress_divergence(gen_dist_spi)
+    species[["divergence"]] <- gen_dist_spi
+    clu_gen_spi_ti_c <- Tdbscan(gen_dist_spi$compressed_matrix,
+                                config$gen3sis$speciation$divergence_threshold,
+                                1)
+    clu_gen_spi_ti <- clu_gen_spi_ti_c[gen_dist_spi$index]
+    n_new_sp <- max(clu_gen_spi_ti) - 1
+    vars$n_new_sp_ti <- vars$n_new_sp_ti + n_new_sp
+    if (n_new_sp > 0) {
+      if (config$gen3sis$general$verbose >= 3) {
+        cat(paste("[!]   Wellcome Strange  Thing   [!] \n"))
+        cat(paste(n_new_sp, "speciation event(s) happened \n"))
+      }
+      desc_unique <- unique(clu_gen_spi_ti)[-1] + vars$n_sp +
+        vars$n_sp_added_ti - 1
+      data$phy <- rbind(data$phy, data.frame(Ancestor = rep(spi,
+                                                            n_new_sp), Descendent = desc_unique, Speciation.Time = rep(vars$ti,
+                                                                                                                       n_new_sp), Extinction.Time = rep(vars$ti, n_new_sp),
+                                             Speciation.Type = rep("Genetic", n_new_sp)))
+      full_gen_dist <- gen_dist_spi
+      gen_dist_spi$index <- gen_dist_spi$index[clu_gen_spi_ti ==
+                                                 1]
+      ue <- unique(gen_dist_spi$index)
+      gen_dist_spi$compressed_matrix <- gen_dist_spi$compressed_matrix[ue,
+                                                                       ue, drop = FALSE]
+      if (length(ue) > 0) {
+        fullrange <- 1:length(ue)
+        dimnames(gen_dist_spi$compressed_matrix) <- list(fullrange,
+                                                         fullrange)
+        for (i in 1:length(gen_dist_spi$index)) {
+          gen_dist_spi$index[i] <- fullrange[ue == gen_dist_spi$index[i]]
+        }
+      }
+      for (desci in desc_unique) {
+        tep_clu_gen_desci_index <- which(desc_unique ==
+                                           desci) + 1
+        new_species <- create_species_from_existing(species,
+                                                    desci, names(species[["abundance"]][clu_gen_spi_ti ==
+                                                                                          tep_clu_gen_desci_index]), config)
+        data$all_species <- append(data$all_species,
+                                   list(new_species))
+      }
+      species <- limit_species_to_cells(species = species,
+                                        cells = names(species[["abundance"]][clu_gen_spi_ti ==
+                                                                               1]))
+      vars$n_sp_added_ti <- vars$n_sp_added_ti + n_new_sp
+      clu_geo_spi_ti <- clu_geo_spi_ti[clu_gen_spi_ti ==
+                                         1]
+    }
+    data$all_species[[spi]] <- species
+  }
+  if (config$gen3sis$general$verbose >= 3) {
+    cat(paste("exiting speciation module \n"))
+  }
+  if (config$gen3sis$general$verbose >= 3 && vars$n_sp_added_ti >
+      0) {
+    cat(paste(vars$n_sp_added_ti, "new species created \n"))
+  }
+  return(list(config = config, data = data, vars = vars))
+}
+
+disperse <- function (species, landscape, distance_matrix, config) 
+{
+  # if(ti == 94){
+  #   browser()
+  # }
+  if (!length(species[["abundance"]])) {
+    return(species)
+  }
+  presence_spi_ti <- names(species[["abundance"]])
+  all_cells <- rownames(landscape$coordinates)
+  free_cells <- all_cells[!(all_cells %in% presence_spi_ti)]
+  num_draws <- length(free_cells) * length(presence_spi_ti)
+  r_disp <- config$gen3sis$dispersal$get_dispersal_values(num_draws, 
+                                                          species, landscape, config)
+  geo_disp <- distance_matrix[presence_spi_ti, free_cells, 
+                              drop = FALSE]
+  geo_disp <- geo_disp <= r_disp
+  colonized <- rep(FALSE, length(all_cells))
+  names(colonized) <- all_cells
+  colonized[free_cells] <- apply(geo_disp, 2, any)
+  tep_occ_id <- all_cells[colonized]
+  if (length(tep_occ_id) > 0) {
+    dest <- which(colonized == TRUE)
+    if (length(presence_spi_ti) == 1) {
+      orig <- rep(1, length(dest))
+    }
+    else {
+      orig <- apply(geo_disp[, colonized[free_cells], 
+                             drop = FALSE], 2, function(x) {
+                               a <- which(x)
+                               ifelse(length(a) > 1, sample(a, 1), a)
+                             })
+    }
+    orig <- as.numeric(presence_spi_ti[orig])
+    dest <- tep_occ_id
+    species <- disperse_species(species, as.character(orig), 
+                                dest, config)
+  }
+  return(species)
+}
+
+
+loop_dispersal <- function (config, data, vars) 
+{
+  # if(ti == 94){
+  #   browser()
+  # }
+  if (config$gen3sis$general$verbose >= 3) {
+    cat(paste("entering dispersal module \n"))
+  }
+  data$all_species <- lapply(data$all_species, disperse, data$landscape, 
+                             data$distance_matrix, config)
+  if (config$gen3sis$general$verbose >= 3) {
+    cat(paste("exiting dispersal module \n"))
+  }
+  return(list(config = config, data = data, vars = vars))
+}
+
 
 #### REMOVING TRAITS OF ANTERIOR SIMULATIONS ####
 caminho <- here("data", "raw", "WorldCenter", "output", "config_worldcenter", "traits")
@@ -622,7 +685,7 @@ for(p in 1:length(plasti)){
     camatualizado[[k]] <- paste(cam[k], listfiles[k], sep = "/", collapse = "--")
   }
   file.remove(camatualizado)  
- rm(val, sgen3sis, rateextinction, ratespeciation, diversification, traitevolution, result, datafinal, datafinal_less_last, datafinal_less_first, list_difference, list_difference2)
+ #rm(val, sgen3sis, rateextinction, ratespeciation, diversification, traitevolution, result, datafinal, datafinal_less_last, datafinal_less_first, list_difference, list_difference2)
 }
 
 path <- here("output")
